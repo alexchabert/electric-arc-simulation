2bd9ab119144a29927ba31458ff2a660774ffb17
alexchabert <87310147+alexchabert@users.noreply.github.com>
2021-07-15 10:24:52 +0200

    Update R_Arc_AC_git.py

--- state_space_sim/R_AC/R_Arc_AC_git.py
+++ state_space_sim/R_AC/R_Arc_AC_git.py
@@ -123,7 +123,7 @@ print('error power on current =',errPI)
 #--------------------------------------------------------------
 plt.close('all')
 plt.rc('font', family='serif', serif='Times')
-plt.rc('text', usetex=True)
+plt.rc('text', usetex=False)
 plt.rc('xtick', labelsize=10)
 plt.rc('ytick', labelsize=10)
 plt.rc('axes', labelsize=10)
